8391b3fcbfc0fc52694ceba170066b98108a6c9c
hernad <hernad@bring.out.ba>
2018-03-12 13:49:33 +0100

    hbwin

--- appveyor.bat
+++ appveyor.bat
@@ -9,24 +9,29 @@ rem Set the paths appropriately
 PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
 
 rem Upgrade the MSYS2 platform
-bash -lc "pacman --noconfirm --sync --refresh --refresh pacman"
-bash -lc "pacman --noconfirm --sync --refresh --refresh --sysupgrade --sysupgrade"
+REM bash -lc "pacman --noconfirm --sync --refresh --refresh pacman"
+REM bash -lc "pacman --noconfirm --sync --refresh --refresh --sysupgrade --sysupgrade"
+bash -xlc "pacman --noconfirm -S curl"
 
 rem Install required tools
-bash -xlc "pacman --noconfirm -S --needed base-devel"
+REM bash -xlc "pacman --noconfirm -S --needed base-devel"
 
 rem Install the relevant native dependencies
-bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-json-c"
-bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-glib2"
-bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-gobject-introspection"
+REM bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-json-c"
+REM bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-glib2"
+REM bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-gobject-introspection"
 
 rem Invoke subsequent bash in the build tree
 cd %APPVEYOR_BUILD_FOLDER%
+bash -xlc "cd /c ; curl -LO https://dl.bintray.com/hernad/windows/hbwin.tar.gz ; tar xvf hbwin.tar.gz"
+PATH C:\hbwin\bin;%PATH%
+
 set CHERE_INVOKING=yes
 
 rem Build/test scripting
 bash -xlc "set pwd"
 bash -xlc "env"
+bash -xlc "hbmk2 -version"
 
 REM bash -xlc "./autogen.sh"
 REM bash -xlc "./configure"
