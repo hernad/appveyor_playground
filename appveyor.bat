404e6da4cf111ee63e51d8467d0a96ee8001fca8
hernad <hernad@bring.out.ba>
2018-03-13 13:16:27 +0100

    BUILD_RELEASE libpq.dll

--- appveyor.bat
+++ appveyor.bat
@@ -15,7 +15,7 @@ set PATH=C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
 rem Upgrade the MSYS2 platform
 REM bash -lc "pacman --noconfirm --sync --refresh --refresh pacman"
 REM bash -lc "pacman --noconfirm --sync --refresh --refresh --sysupgrade --sysupgrade"
-bash -xlc "pacman --noconfirm -S curl zip"
+bash -xlc "pacman --noconfirm -S curl zip unzip"
 
 rem Install required tools
 REM bash -xlc "pacman --noconfirm -S --needed base-devel"
@@ -43,4 +43,12 @@ REM bash -xlc "make distcheck"
 
 REM bash -xlc "gcc --version"
 
-bash -xlc "export MSYS2=c:\\msys64\\mingw32 ; export HB_WITH_PGSQL=$MSYS2\\include; export HB_ARCHITECTURE=win; export HB_COMPILER=mingw; hbmk2 hello.hbp ; zip hello_${BUILD_ARTIFACT}_${APPVEYOR_REPO_TAG_NAME}.zip hello.exe"
+REM build hello.hbp
+bash -xlc "export MSYS2=c:\\msys64\\mingw32 ; export HB_WITH_PGSQL=$MSYS2\\include; export HB_ARCHITECTURE=win; export HB_COMPILER=mingw; hbmk2 hello.hbp"
+
+REM postgresql dlls libpq.dll i kompanija
+bash -xlc "curl -LO https://dl.bintray.com/hernad/F18/postgresql_windows_x86_dlls.zip; unzip postgresql_windows_x86_dlls.zip"
+
+REM run hello.exe
+bash -xlc "./hello.exe ; zip hello_${BUILD_ARTIFACT}_${APPVEYOR_REPO_TAG_NAME}.zip hello.exe"
+
