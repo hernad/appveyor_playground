0be622576a4f4443b5ae6168b1bdc50b334c0c77
hernad <hernad@bring.out.ba>
2018-03-13 10:11:08 +0100

    BUILD_RELEASE hb_linux_i386

--- appveyor.sh
+++ appveyor.sh
@@ -8,4 +8,11 @@ echo "artifakt: $BUILD_ARTIFACT tag: $APPVEYOR_REPO_TAG_NAME"
 
 gcc --version
 
-zip hello_${BUILD_ARTIFACT}_${APPVEYOR_REPO_TAG_NAME}.zip hello.prg
+curl -LO https://dl.bintray.com/hernad/harbour/hb-linux-i386.tar.gz
+tar xf hb-linux-i386.tar.gz
+export PATH=$(pwd)/hb-linux-i386/bin:$PATH
+
+hbmk2 -version
+hbmk2 -gtstd hello.prg
+./hello
+zip hello_${BUILD_ARTIFACT}_${APPVEYOR_REPO_TAG_NAME}.zip hello
