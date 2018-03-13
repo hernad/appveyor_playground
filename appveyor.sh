784889fb752adcfc94b9da1841e841d9099fd2f1
hernad <hernad@bring.out.ba>
2018-03-13 08:55:04 +0100

    update

--- appveyor.sh
+++ appveyor.sh
@@ -2,6 +2,8 @@
 
 echo "hello world"
 
-sudo apt install -y zip
+#sudo apt install -y zip
+
+echo "artifakt: $BUILD_ARTIFACT tag: $APPVEYOR_REPO_TAG_NAME"
 
 zip hello_${BUILD_ARTIFACT}_${APPVEYOR_REPO_TAG_NAME}.zip hello.prg
